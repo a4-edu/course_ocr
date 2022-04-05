e6e25ddc132fa05468436ce9d4664a9e232df497
Dmitry Zvonarev <dmitry.zvonarev@abbyy.com>
2022-04-05 09:21:45 +0300

    Добавил setup, немного ухудшил пример

---
+++ task3/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup, find_packages
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+setup(
+    name='course_ocr_t3',
+    version='0.1.0',
+    python_requires='>=3.7.0',
+    packages=[''],
+    description='Task3 for ABBYY OCR course',
+    url='',
+    author='Dmitry Zvonarev',
+    author_email='dmitry.zvonarev@abbyy.com',
+    install_requires=['shapely'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Scientific/Engineering'
+    ],
+)
